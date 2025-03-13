32f525d5f6f115a156ecc6617c3d8e186cf7ecfb
evertonprdo <ecpj.prado@gmail.com>
2025-03-13 17:31:17 -0300

    before OS migration

--- MyFirstAppAndroid/app/build.gradle.kts
+++ MyFirstAppAndroid/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.evertonprdo.myfirstappandroid"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.evertonprdo.myfirstappandroid"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
